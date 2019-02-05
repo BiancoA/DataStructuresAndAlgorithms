dcda499356d4044e7142186a6357f502f9ce9f21
BiancoA <andrea.bianco@truvis.ch>
2019-02-05 16:01:23 +0100

    [ADD] search

--- tree.cpp
+++ tree.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
+#include <vector>
+#include <iterator>
 
 struct Node
 {
-        Node(double value):
-        left(nullptr),
-        right(nullptr),
-        nodeValue(value)
+        Node(double value) :
+                left(nullptr),
+                right(nullptr),
+                nodeValue(value)
         {
-         std::cout<<"node value: "<<nodeValue<<"\n";
+                //std::cout<<"node value: "<<nodeValue<<"\n";
         };
         friend std::ostream& operator << (std::ostream& os, Node* node){
-            os<<"("<<node->nodeValue<<"("<<node->left->nodeValue<<"("<<node->right->nodeValue<<"))";
-            return os;
+                os<<"("<<node->nodeValue<<"("<<node->left->nodeValue<<"("<<node->right->nodeValue<<"))";
+                return os;
         };
 
         Node *left;
@@ -21,60 +23,57 @@ struct Node
 
 class BinaryTree
 {
-  public:
-        BinaryTree(double Value):
+public:
+BinaryTree(double Value) :
         originalRoot(nullptr){
-          originalRoot = InternalAdd(originalRoot, Value);
-        };
+        originalRoot = InternalAdd(originalRoot, Value);
+};
 
-        void Add(double Value){
-          InternalAdd(originalRoot, Value);
-        }
+void Add(double Value){
+        InternalAdd(originalRoot, Value);
+}
 
-        void Search(double Value){
-          InternalSearch(originalRoot, Value);
-        }
+void Search(double Value){
+        InternalSearch(originalRoot, Value);
+}
 
-      private:
-        bool InternalSearch(Node* root, double Value){
-            if(root->nodeValue == Value){
+private:
+bool InternalSearch(Node* root, double Value){
+        if(root==nullptr) {
+                std::cout<<"Not found"<<"\n";
+                return false;
+        }else if(root->nodeValue == Value) {
                 std::cout<<"found! at node "<< root<<"\n";
                 return true;
-            }else if(root->nodeValue < Value){
+        }else if(root->nodeValue < Value) {
                 return InternalSearch(root->left, Value);
-            }else if(root->nodeValue > Value){
+        }else if(root->nodeValue > Value) {
                 return InternalSearch(root->right, Value);
-            }else{
-              std::cout<<"Not found"<<"\n";
-              return false;
-            }
         }
+        return false;
+}
 
-        Node* InternalAdd(Node* root, double Value){
-            //we found a leaf
-            if(root==nullptr){
+Node* InternalAdd(Node* root, double Value){
+        //we found a leaf
+        if(root==nullptr) {
                 root = new Node(Value);
-            }else if(root->nodeValue < Value){
+        }else if(root->nodeValue < Value) {
                 root->left = InternalAdd(root->left, Value);
-            }else if(root->nodeValue > Value){
+        }else if(root->nodeValue > Value) {
                 root->right = InternalAdd(root->right, Value);
-            }
-            return root;
         }
+        return root;
+}
 
-        Node *originalRoot;
+Node *originalRoot;
 
 };
 
 int main(){
-    BinaryTree tree(19.1);
+        std::vector<double> v = {1,4,32.01,123.3,45.5,12,34,342.01,13.3,455.5,-12};
+        BinaryTree tree(19.1);
 
-    tree.Add(10);
-    tree.Add(1.0);
-    tree.Add(42);
-    tree.Add(12.9);
-    tree.Add(89.999);
-    tree.Add(32.0);
-
-    tree.Search(42);
+        for()
+        tree.Search(42);
+        tree.Search(8);
 }
