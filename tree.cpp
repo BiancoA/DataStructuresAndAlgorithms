f76c1d9f6ab61e7d164e04256aa9907f023a2db6
BiancoA <andrea.bianco@truvis.ch>
2019-02-07 16:38:27 +0100

    [ADD] remove function still some bugs

--- tree.cpp
+++ tree.cpp
@@ -24,6 +24,9 @@ struct Node
                 return os;
 
         };
+        ~Node(){
+          std::cout<<"deleted\n";
+        }
 
         Node *left;
         Node *right;
@@ -44,6 +47,12 @@ void Add(double Value){
 
 void Search(double Value){
         InternalSearch(originalRoot, Value);
+}
+
+void Remove(double Value){
+    if(InternalSearch(originalRoot, Value)){
+        InternalRemove(originalRoot, Value);
+    }
 }
 
 private:
@@ -74,12 +83,52 @@ Node* InternalAdd(Node*& root, double Value){
         return root;
 }
 
+void InternalRemove(Node*& node, double Value){
+      if(node->nodeValue < Value){
+          InternalRemove(node->left, Value);
+      }else if(node->nodeValue > Value){
+          InternalRemove(node->right, Value);
+      }else{
+          if(node->left==nullptr && node->right==nullptr){
+            //leaf
+            delete node;
+          }else if(node->left==nullptr){
+            node = node->right;
+            delete node->right;
+          }else if(node->right==nullptr){
+            node = node->left;
+            delete node->left;
+          }else{
+            //both right and left are present
+            Node* leftmost = FindMin(node->right);
+            std::cout<<leftmost;
+            node->nodeValue = leftmost->nodeValue;
+            if(leftmost->right == nullptr){
+              delete leftmost;
+            }else{
+              leftmost->nodeValue = leftmost->right->nodeValue;
+              delete leftmost->right;
+            }
+
+          }
+
+      }
+
+}
+Node* FindMin(Node*& node){
+  if(node->left ==nullptr){
+    return node;
+  }else{
+    return FindMin(node->left);
+  }
+}
+
 Node *originalRoot;
 
 };
 
 int main(){
-        std::vector<double> v = {1,42,32.01,123.3,45.5,12,34,342.01,13.3,455.5,-12,8};
+        std::vector<double> v = {1,42,-22,32.01,123.3,45.5,12,34,342.01,13.3,455.5,-12,8};
         BinaryTree tree(19.1);
 
         for(std::vector<double>::iterator it=v.begin(); it<v.end(); it++) {
@@ -89,6 +138,15 @@ int main(){
         tree.Search(8);
         tree.Search(19.1);
         tree.Search(12);
+        tree.Search(1);
+
+        tree.Remove(42);
+        tree.Search(42);
+
+        tree.Remove(8);
+        tree.Search(8);
+
+        tree.Remove(1);
         tree.Search(1);
 
 }
