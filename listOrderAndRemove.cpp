e0353758844d391545d091dc00e5379ce5fec183
BiancoA <biancoand@gmail.com>
2019-03-06 16:05:55 +0100

    [ADD] list

--- listOrderAndRemove.cpp
+++ listOrderAndRemove.cpp
@@ -1,10 +1,12 @@
+#include <iostream>
 class Node{
 public:
     int value;
     Node *prev;
     Node *next;
 
-    Node(int value){
+    Node(int Value){
+      value = Value;
       prev = nullptr;
       next = nullptr;
     }
@@ -23,7 +25,35 @@ class List{
       }
       void Add(int value){
         Node* last = root;
-
+        while(last->next!=nullptr){
+          last=last->next;
+        }
+        last->next = new Node(value);
+        last->next->prev = last;
+      }
+      void Remove(int value){
+        Node* last = root;
+        if(last->value == value){
+          root = last->next;
+          return;
+        }
+        
+        while(last->value!=value){
+          last=last->next;
+        }
+        Node* tmp = last;
+        last->prev->next = tmp->next;
+        if(tmp->next != nullptr)
+          last->next->prev = tmp->prev;
+      }
+      void Print(){
+        Node* last = root;
+        std::cout<<"("<<last->value<<") -> (";
+        while(last->next!=nullptr){
+          last=last->next;
+          std::cout<<last->value<<") -> (";
+        }
+        std::cout<<"null )\n";
       }
 
     private:
@@ -32,5 +62,18 @@ class List{
 };
 
 int main(){
-  
+  List l(10);
+  l.Print();
+  l.Add(1);
+  l.Print();
+  l.Add(4);l.Add(5);l.Add(9);l.Add(2);l.Add(0);
+  l.Print();
+  l.Remove(4);
+  l.Print();
+  l.Remove(0);
+  l.Print();
+  l.Remove(10);
+  l.Print();
+
+
 }
