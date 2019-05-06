eb74a2444e8ee0f938f7633804e3ce3411be3ed1
BiancoA <biancoand@gmail.com>
2019-05-06 14:50:56 +0200

    [ADD] performance study

--- sortStack.cpp
+++ sortStack.cpp
@@ -1,5 +1,6 @@
 #include <stack>
 #include <iostream>
+#include <chrono>
 
 std::stack<int> sortStack(std::stack<int>& inS){
   std::stack<int> tmpS;
@@ -40,17 +41,52 @@ std::stack<int> sortStack(std::stack<int>& inS){
   return tmpS;
 }
 
+std::stack<int> sortStackRevisited(std::stack<int>& inS){
+  std::stack<int> tmpS;
+  int tmpV=0;
+
+  while(!inS.empty()){
+    tmpV = inS.top();
+    inS.pop();
+    //reverse the stack until we find the item that is smaller
+      while(!tmpS.empty() && tmpS.top()>tmpV){
+          inS.push(tmpS.top());
+          tmpS.pop();
+      }
+      tmpS.push(tmpV);
+    }
+
+  return tmpS;
+}
 int main(){
 
-  std::stack<int> s({1,0,123,3,5,89,23});
+using namespace std::chrono;
+  std::stack<int> s1({1,0,123,3,5,89,23,12,1000,5,6,7,34,16,88,96,2,16,1000000});
+
+  std::stack<int> s({1,0,123,3,5,89,23,12,1000,5,6,7,34,16,88,96,2,16,1000000});
+
+  auto t1 = high_resolution_clock::now() ;
 
   std::stack<int> ordered = sortStack(s);
+
+  auto t2 = high_resolution_clock::now() ;
+
+  std::stack<int> ordered2 = sortStackRevisited(s1);
+
+  auto t3 = high_resolution_clock::now() ;
 
   while(!ordered.empty()){
   std::cout<<ordered.top()<< " ";
   ordered.pop();
   }
   std::cout<<"\n";
+
+  while(!ordered2.empty()){
+  std::cout<<ordered2.top()<< " ";
+  ordered2.pop();
+  }
+  std::cout<<"\n";
+  std::cout<<duration_cast<nanoseconds>(t2-t1).count()<<" "<<duration_cast<nanoseconds>(t3-t2).count()<<"\n";
 }
 
 /*
